8f69dbf259457101e2cc026a2b17864dd16a1058
ditas <ditas@DitaRizqie>
2024-07-18 09:07:36 +0700

    Third Push

--- Scripts/TCHitungProperti/Script1720790476786.groovy
+++ Scripts/TCHitungProperti/Script1720790476786.groovy
@@ -27,96 +27,99 @@ int rowData
 int autoSelect
 
 for (rowData = 1; rowData <= findTestData('HitungProperti/TDHitungProperti').getRowNumbers(); rowData++) {
-	
-	long pemasukan = Long.parseLong(findTestData('HitungProperti/TDHitungProperti').getValue('Penghasilan', rowData))
+    long pemasukan = Long.parseLong(findTestData('HitungProperti/TDHitungProperti').getValue('Penghasilan', rowData))
 
     long pengeluaran = Long.parseLong(findTestData('HitungProperti/TDHitungProperti').getValue('Pengeluaran', rowData))
-	
-	if(pemasukan != '' && pengeluaran !=''){
 
-		autoSelect = (new Random().nextInt(30) + 1)
-		
-		WebUI.focus(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPemasukan'))
-		
-		WebUI.setText(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPemasukan'), pemasukan.toString())
+    if ((pemasukan != '') && (pengeluaran != '')) {
+        autoSelect = (new Random().nextInt(30) + 1)
 
-		WebUI.focus(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPengeluaran'))
+        WebUI.focus(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPemasukan'))
+
+        WebUI.setText(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPemasukan'), pemasukan.toString())
+
+        WebUI.focus(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPengeluaran'))
+
+        WebUI.setText(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPengeluaran'), pengeluaran.toString())
+
+        WebUI.selectOptionByValue(findTestObject('Page Hitung Harga Properti Maksimal BTN/dropdownTahun'), autoSelect.toString(), 
+            false)
+
+        
+        
+		def tpTestCase = findTestData('HitungProperti/TDHitungProperti').getValue('TypeTest', rowData)
+
+        String nameCapture = (('TestHitungBTN - Script ' + tpTestCase) + ' row ') + rowData
+
+        if (tpTestCase == 'Positive') {
+			WebUI.scrollToElement(findTestObject('Page Hitung Harga Properti Maksimal BTN/dropdownTahun'), 30)
 			
-		WebUI.setText(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPengeluaran'), pengeluaran.toString())
-		
-		WebUI.selectOptionByValue(findTestObject('Page Hitung Harga Properti Maksimal BTN/dropdownTahun'), autoSelect.toString(), 
-			false)
-		
+            WebUI.focus(findTestObject('Page Hitung Harga Properti Maksimal BTN/Button Hitung'))
+
+            WebUI.click(findTestObject('Page Hitung Harga Properti Maksimal BTN/Button Hitung'))
+
+            WebUI.focus(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPemasukan'))
+
+            String txtHitungReal = WebUI.getText(findTestObject('Page Hitung Harga Properti Maksimal BTN/TextHasilKalkulasi'))
+
+            long numHitungReal = Long.parseLong(txtHitungReal.replaceAll('[^\\d]', ''))
+
+            long expectedHitung = ((pemasukan - pengeluaran) * (autoSelect * 12)) / 3
+
+            try {
+                assert numHitungReal == expectedHitung
+
+                WebUI.takeScreenshot(('D:\\' + nameCapture) + ' - success.png')
+            }
+            catch (AssertionError e) {
+                WebUI.takeScreenshot(('D:\\' + nameCapture) + ' - failed.png')
+            } 
+        } else {
+            String actualTextValidasi = WebUI.getText(findTestObject('Page Hitung Harga Properti Maksimal BTN/msg Validasi Pengeluaran Lebih Besar'))
+
+			WebUI.scrollToElement(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPemasukan'), 30)
+			
+            try {
+                assert actualTextValidasi == 'Isi kurang dari nilai sebelumnya'
+				
+				WebUI.takeScreenshot(('D:\\' + nameCapture) + ' - success 01.png')
+				
+				WebUI.delay(60)
+							
+				try {
+					WebUI.scrollToElement(findTestObject('Page Hitung Harga Properti Maksimal BTN/dropdownTahun'), 30)
+					
+					WebUI.verifyElementNotClickable(findTestObject('Page Hitung Harga Properti Maksimal BTN/Button Hitung'))
+	
+					WebUI.takeScreenshot(('D:\\' + nameCapture) + ' - success 02.png')
+				}
+				catch (Exception e) {
+					WebUI.takeScreenshot(('D:\\' + nameCapture) + ' - failed.png')
+				}
+            }
+            catch (AssertionError e) {
+				WebUI.takeScreenshot(('D:\\' + nameCapture) + ' - failed.png')
+            } 
+            
+            
+        }
+        
+        WebUI.refresh()
+    } else {
 		WebUI.scrollToElement(findTestObject('Page Hitung Harga Properti Maksimal BTN/dropdownTahun'), 30)
 		
-		def tpTestCase = findTestData('HitungProperti/TDHitungProperti').getValue('TypeTest', rowData)
-		
-		String nameCapture = 'TestHitungBTN - Script ' + tpTestCase + ' row ' + rowData
-		
-		if(tpTestCase == 'Positive') {
-			WebUI.focus(findTestObject('Page Hitung Harga Properti Maksimal BTN/Button Hitung'))
-			
-			WebUI.click(findTestObject('Page Hitung Harga Properti Maksimal BTN/Button Hitung'))
-						
-			WebUI.focus(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPemasukan'))
-			
-			String txtHitungReal = WebUI.getText(findTestObject('Page Hitung Harga Properti Maksimal BTN/TextHasilKalkulasi'))
+        try {
+            WebUI.verifyElementNotClickable(findTestObject('Page Hitung Harga Properti Maksimal BTN/Button Hitung'))
 
-			long numHitungReal = Long.parseLong(txtHitungReal.replaceAll('[^\\d]', ''))
-
-			long expectedHitung = ((pemasukan - pengeluaran) * (autoSelect * 12)) / 3
-
-			
-			try {
-				assert numHitungReal == expectedHitung
-
-				WebUI.takeScreenshot('D:\\'+ nameCapture +' - success.png')
-				
-			}
-			catch (AssertionError e) {
-				WebUI.takeScreenshot('D:\\'+ nameCapture +' - failed.png')
-			} 
-			
-			
-		} else {
-			WebUI.focus(findTestObject('Page Hitung Harga Properti Maksimal BTN/txtPemasukan'))
-			
-			String actualTextValidasi = WebUI.getText(findTestObject('Page Hitung Harga Properti Maksimal BTN/msg Validasi Pengeluaran Lebih Besar'))
-			
-			try {
-				assert actualTextValidasi == 'Isi kurang dari nilai sebelumnya'
-
-				
-			}
-			catch (AssertionError e) {
-				
-			}
-			
-			try {
-				WebUI.verifyElementNotClickable(findTestObject('Page Hitung Harga Properti Maksimal BTN/Button Hitung'))
-
-				WebUI.takeScreenshot('D:\\'+ nameCapture +' - success.png')
-			}
-			catch (Exception e) {
-				WebUI.takeScreenshot('D:\\'+ nameCapture +' - failed.png')
-			} 
-			
-		}
-		
-		WebUI.refresh()
-		
-	} else {
-		try {
-			WebUI.verifyElementNotClickable(findTestObject('Page Hitung Harga Properti Maksimal BTN/Button Hitung'))
-
-			WebUI.takeScreenshot('D:\\'+ nameCapture +' - success.png')
-			
-		}	catch (Exception e) {
-		
-			WebUI.takeScreenshot('D:\\'+ nameCapture +' - failed.png')
-		
-		}
-	}
+            WebUI.takeScreenshot(('D:\\' + nameCapture) + ' - success.png')
+        }
+        catch (Exception e) {
+            WebUI.takeScreenshot(('D:\\' + nameCapture) + ' - failed.png')
+        } 
+    }
 }
 
 WebUI.closeBrowser(FailureHandling.CONTINUE_ON_FAILURE)
+
+
+
